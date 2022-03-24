08136e0386e7cc50a2c303d65a4b3ce55c2a77bd
Olli Tietäväinen <ollit@vaadin.com>
2022-03-24 13:35:23 +0200

    initial commit

--- src/main/java/com/example/application/views/about/AboutView.java
+++ src/main/java/com/example/application/views/about/AboutView.java
@@ -7,6 +7,13 @@ import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.InputStreamFactory;
+import com.vaadin.flow.server.StreamResource;
+import org.vaadin.olli.FileDownloadWrapper;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @PageTitle("About")
 @Route(value = "about", layout = MainLayout.class)
@@ -14,11 +21,17 @@ public class AboutView extends VerticalLayout {
 
     public AboutView() {
         setSpacing(false);
-
         Image img = new Image("images/empty-plant.png", "placeholder plant");
         img.setWidth("200px");
-        add(img);
-
+        //add(img);
+        FileDownloadWrapper wrapper = new FileDownloadWrapper(new StreamResource("foo.txt", new InputStreamFactory() {
+            @Override
+            public InputStream createInputStream() {
+                return new ByteArrayInputStream("foo".getBytes(StandardCharsets.UTF_8));
+            }
+        }));
+        wrapper.wrapComponent(img);
+        add(wrapper);
         add(new H2("This place intentionally left empty"));
         add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
 
